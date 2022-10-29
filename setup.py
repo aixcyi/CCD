8b3bbe7dd8829effe95f16e7ce69e5f5b3f40300
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2022-10-29 15:24:40 +0800

    build: 添加打包配置及开源许可证

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyCLC',
+    version='0.1.0',
+    packages=['py_clc'],
+    url='https://github.com/aixcyi/PyCLC',
+    license='MIT',
+    author='aixcyi',
+    author_email='75880483+aixcyi@users.noreply.github.com',
+    description='A python util package about Chinese lunisolar calendar.'
+)
