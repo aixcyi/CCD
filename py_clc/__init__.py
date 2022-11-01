fdcd4759e83d28313f0506c3ba4c1242d142f4eb
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2022-11-01 15:07:43 +0800

    refactor: 将limited合并到base，不再提供抽象类

--- py_clc/__init__.py
+++ py_clc/__init__.py
@@ -2,4 +2,4 @@ try:
     import ephem
     from py_clc.calc_ephem import ChineseCalendarDate
 except ImportError:
-    from py_clc.limited import ChineseCalendarDate
+    from py_clc.base import ChineseCalendarDate
